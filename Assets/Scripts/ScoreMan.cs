bed39f60aa0af4b9ac8cd4a9d55ea219a1d35cf2
RandomDucks <RandomDucks@users.noreply.github.com>
2017-04-22 22:58:11 +0200

    Added a ScoreManeger

---
+++ Assets/Scripts/ScoreMan.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreMan : MonoBehaviour {
+public Text LivesText, MoneyText;
+public int lives = 20, money = 500;
+	// Use this for initialization
+	void Start () {
+		LivesText.text = "Lives: " + lives.ToString();
+		MoneyText.text = "Quillings: " + money.ToString();
+	}
+	
+	// Update is called once per frame
+	void UpdateText () {
+		LivesText.text = "Lives: " + lives.ToString();
+		MoneyText.text = "Quillings: " + money.ToString();
+	}
+	public void LoseLife (int i) {
+		lives -= i;
+		UpdateText();
+	}
+	public void GainMoney(int i) {
+		money += i;
+		UpdateText();
+	}
+	public void LoseMoney(int i) {
+		money -= i;
+		UpdateText();
+	}
+
+}
